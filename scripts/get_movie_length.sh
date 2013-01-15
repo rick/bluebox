da6d7a6003877f1931319fdf3af7089bc7c09d4f
Rick Bradley <rick@rickbradley.com>
2013-01-15 14:20:45 -0800

    a version get_movie_length for OSX mplayer2

--- scripts/get_movie_length.sh
+++ scripts/get_movie_length.sh
@@ -1,2 +1,2 @@
-LENGTH=`/usr/bin/mplayer -vc null -vo null -identify -frames 0 "$*" 2>&1 | grep ID_LENGTH | sed 's:^.*=::'`
+LENGTH=`/Applications/mplayer2.app/Contents/MacOS/mplayer2 -vc null -vo null -identify -frames 0 "$*" 2>&1 | grep ID_LENGTH | sed 's:^.*=::'`
 echo "$LENGTH	$*"
