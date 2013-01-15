0bde0f1f00beaa5f12057fcfa435438a1e315323
Rick Bradley <rick@rickbradley.com>
2013-01-15 14:19:17 -0800

    an osx version of mplayer play_movie

--- scripts/play_movie.sh
+++ scripts/play_movie.sh
@@ -1,5 +1,4 @@
-chvt 3
-subtitle=`ls -tr /art/subtitles | tail -1`
-/usr/bin/mplayer -quiet -osdlevel 0 -nojoystick -nolirc -nomouseinput -noconsolecontrols -subfont-outline 8 -idx -nooverlapsub -framedrop -menu -sub /art/subtitles/${subtitle} -vo fbdev2 -vf scale=800:600:1 -subpos 92 -double -ss $2 -nosound  $1 >>/art/logs/master.log &
-sleep $3
-killall mplayer
+# subtitle=`ls -tr /art/subtitles | tail -1`
+/Applications/mplayer2.app/Contents/MacOS/mplayer2 -quiet -nomouseinput -noconsolecontrols -vo gl -vf scale=800:600:1 -fs -ss $2 -nosub "$1"
+#sleep $3
+#killall mplayer2
